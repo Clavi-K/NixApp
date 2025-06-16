64d113ec10c3830996c08603c4030079c28ec728
Luciano Schmidt <luciano43schmidt@gmail.com>
2025-06-16 20:45:56 -0300

    creacion de poc de logout modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,19 @@
+"use client"
 
-export default function Modal() {
-    return (<dialog id="modalTest" className="modal">
+export default function Modal(params) {
+
+
+    const handleCloseBtn = () => {
+        params.modalState.setModalClass("")
+    }
+
+    return (<dialog id="modalTest" className={`modal ${params.modalState.modalClass}`}>
         <div className="modal-box">
             <h3 className="text-lg font-bold">Confirmation</h3>
             <p className="py-4">Are you sure you want to log out?</p>
             <div className="modal-action">
                 <form method="dialog">
-                    <button className="btn">Close</button>
+                    <button className="btn" onClick={handleCloseBtn}>Close</button>
                 </form>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,28 @@ import { LogOut } from "lucide-react"
 import { logoutUser } from "@/app/actions"
 import { redirect } from "next/navigation"
 import { useAuth } from "@/context/AuthContext"
+import { useState } from "react"
+
+import Modal from "./Modal"
 
 export default function Navbar() {
+
     const { user, setUser } = useAuth()
+    const [modalClass, setModalClass] = useState("")
 
     const handleLogoutBtn = async () => {
 
-        const userToken = localStorage.getItem("nixAccessToken")
+        setModalClass("modal-open")
 
-        if (userToken) {
-            await logoutUser(userToken)
-            localStorage.removeItem("nixAccessToken")
-            setUser(null)
-        }
-
-        redirect("/user/login")
+        /*  const userToken = localStorage.getItem("nixAccessToken")
+ 
+         if (userToken) {
+             await logoutUser(userToken)
+             localStorage.removeItem("nixAccessToken")
+             setUser(null)
+         }
+ 
+         redirect("/user/login") */
     }
 
     return (<div className="navbar bg-navbar shadow-lg ">
@@ -27,10 +34,14 @@ export default function Navbar() {
             <h3 className="text-2xl text-secondary font-bold">Nix_</h3>
         </div>
         <div className="navbar-center"></div>
-        <div className="navbar-end" hidden={!user}>
+
+        {/* <div className="navbar-end" hidden={!user}> */}
+        <div className="navbar-end" hidden={false}>
             <p className="mr-5 transition-all transition-discrete hover:text-primary">{user?.name}</p>
             <LogOut className="transition-all transition-discrete hover:text-error mr-3" onClick={handleLogoutBtn} />
         </div>
+
+        <Modal title="test" text="a" callback="" modalState={{ modalClass, setModalClass }} />
 
     </div>)
 }
